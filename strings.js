20a7954e19e241ed43f869d813e86379b78ebd69
Sefap <85367995+Sefap@users.noreply.github.com>
2022-03-23 00:13:57 +0300

    Create strings.js

---
+++ strings.js
@@ -0,0 +1,8 @@
+//Stringler tek tırnak çift tırnak veya ters tırnakla tanımlanan tüm değerlerdir.
+let space = ' '           // boş string
+let firstName = 'Sefa'
+let lastName = 'Palabıyık'
+let country = 'Türkiye'
+let language = 'JavaScript'
+let quote = "Herzaman daha iyiye."
+let quotWithBackTick = `Herzaman daha iyiye.`
