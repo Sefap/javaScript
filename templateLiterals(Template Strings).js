afc58db50f820a0f283329b7e02da2fcc3ff2cde
Sefap <85367995+Sefap@users.noreply.github.com>
2022-03-23 00:15:07 +0300

    Create templateLiterals(Template Strings).js

---
+++ templateLiterals(Template Strings).js
@@ -0,0 +1,14 @@
+let a = 5
+let b = 7
+console.log(`The sum of ${a} and ${b} is ${a + b}`) // dinamik yazım şekli
+
+let firstName = 'Sefa'
+let lastName = 'Palabıyık'
+let country = 'Türkiye'
+let language = 'JavaScript'
+let fullName = firstName + ' ' + lastName
+
+let personInfo = `I am ${fullName}. I live in ${country}. I teach ${language}.` 
+
+console.log(personInfo)
+
