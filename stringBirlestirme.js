4fcdb0361fd2af44d0bcd15ece26a71f0c23e7c0
Sefap <85367995+Sefap@users.noreply.github.com>
2022-03-23 00:13:42 +0300

    Create stringBirlestirme.js

---
+++ stringBirlestirme.js
@@ -0,0 +1,16 @@
+let space = ' '           
+let firstName = 'Sefa'
+let lastName = 'Palabıyık'
+let country='Turkey'
+let language='JavaScript'
+let job='Student'
+let age=23
+
+let fullName = firstName + space + lastName; // iki string değeri birleştirme
+console.log(fullName);
+
+
+//farklı veri türlerini birleştirme
+
+let personInfo=fullName+'. I am '+age+'. I live in ' + country;
+console.log(personInfo)
