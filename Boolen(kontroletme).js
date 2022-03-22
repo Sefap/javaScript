9fe83b2018bad34b5888d2eb669f4ca3a7493092
Sefap <85367995+Sefap@users.noreply.github.com>
2022-03-23 00:11:41 +0300

    Create Boolen(kontroletme).js

---
+++ Boolen(kontroletme).js
@@ -0,0 +1,35 @@
+//Kontrol etme işlemleri içi kullanılır sadece true veya false sonuç döner
+//Örnek
+
+let num1=3
+let num2=3
+console.log(num1==num2) //true 
+// console true yazar
+
+
+let js='JavaScript'
+let ht='HTML'
+console.log(js==ht) // false
+//console false yazar
+
+let nums=[1,2,3]
+let numbers=[1,2,3]
+console.log(num==number) //false
+
+
+
+let nums=[1,2,3]
+let numbers=[1,2,3]
+numbers=nums
+console.log(number== nums) //True
+
+
+let userOne = {
+name:'Sefa',
+role:'Student',
+country:'Türkiye'
+}
+ 
+let userTwo = userOne
+
+console.log(userOne == userTwo)  // true
