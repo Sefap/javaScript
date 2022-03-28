e2cb04e05c6c9d4a6325d931c803cc3e0dc1b294
Sefap <85367995+Sefap@users.noreply.github.com>
2022-03-28 22:43:05 +0300

    Update strings.js

--- strings.js
+++ strings.js
@@ -6,3 +6,17 @@ let country = 'Türkiye'
 let language = 'JavaScript'
 let quote = "Herzaman daha iyiye."
 let quotWithBackTick = `Herzaman daha iyiye.`
+
+
+
+let job='Student'
+let age=23
+
+let fullName = firstName + space + lastName; // iki string değeri birleştirme
+console.log(fullName);
+
+
+//farklı veri türlerini birleştirme
+
+let personInfo=fullName+'. I am '+age+'. I live in ' + country;
+console.log(personInfo)
