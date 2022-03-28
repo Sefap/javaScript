4b81fed8240b145a8fbe25449a4328ef3b850898
Sefap <85367995+Sefap@users.noreply.github.com>
2022-03-28 23:11:35 +0300

    Update templateLiterals(Template Strings).js

--- Basic/templateLiterals(Template Strings).js
+++ Basic/templateLiterals(Template Strings).js
@@ -12,3 +12,42 @@ let personInfo = `I am ${fullName}. I live in ${country}. I teach ${language}.`
 
 console.log(personInfo)
 
+
+
+
+const name = "Sefa Palabıyık"
+const department = "Student"
+const salary = undefined
+
+
+// const a = "İsim: "+ name + "\n"+"Departman: "+department+"\n"+"Salary: "+salary;
+
+
+// const a = `İsim:${name}\nDepartman:${department}\nMaaş:${salary}`
+
+
+// const html = "<ul> " +
+//     "<li>" + name + "</li>"+
+// "<li>" + department + "</li>"+
+// "<li>" + salary + "</li>" +
+//     "</ul>";
+
+
+function a(){
+    return "Merhaba";
+}
+
+
+const html = `
+    <ul>
+        <li>${name}</li>
+        <li>${department}</li>
+        <li>${salary}</li>
+        <li>${10/4}</li>
+        <li>${a()}</li>
+    </ul>
+            `;
+
+
+
+document.body.innerHTML = html;
