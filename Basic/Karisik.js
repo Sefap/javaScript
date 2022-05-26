92e8263aaeb6dd7d4c6dd8ea5d6bc5065ab663a7
Sefa <85367995+sefaplbyk@users.noreply.github.com>
2022-05-27 02:59:01 +0300

    Update Karisik.js

--- Basic/Karisik.js
+++ Basic/Karisik.js
@@ -1,3 +1,9 @@
 console.warn("Bu bir uyarıdır."); 
 console.error("Bu sayfa bulunamadı.");
 console.clear();
+
+
+**Set Fonksiyonu **
+  Mesela elimizde sıralı bir dizi var ama en büyük veya en küçük eleman 2 tane olabilir bu durumlarda o ögeyi setlersek
+  set fonksiyonu içinde aynı değerden 2 tane olmasına izin vermez o yüzden tek 1 en küçük veya en büyük kalacaktır
+  sonrasında o elemanı tekrar arraya çeviririz Array.from fonksiyonu ile ve istediğimiz değere erişebiliriz
