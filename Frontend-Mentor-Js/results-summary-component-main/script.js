f1b88832e3342c50935ae8901e73461179758a94
sefaplbyk <sefa__2095@hotmail.com>
2023-07-19 14:43:03 +0300

    update

---
+++ Frontend-Mentor-Js/results-summary-component-main/script.js
@@ -0,0 +1,22 @@
+
+const ul = document.querySelector('.summary-list-js')
+
+
+fetch('data.json')
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(item => {
+      const li = document.createElement('li');
+      li.classList.add(item.category.toLowerCase())
+      const html = `<div>
+      <img src="${item.icon}" alt="">
+      <span>${item.category}</span>
+      </div>
+      <div><span class="score-rate">${item.score} <span>/ 100</span></span></div>
+      `
+      li.innerHTML=html
+      ul.appendChild(li);
+    });
+    console.log(data);
+  })
+  .catch(error => console.log(error));
