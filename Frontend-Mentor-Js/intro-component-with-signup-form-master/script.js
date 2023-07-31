bc340471ca7806c2ee2b5b38405f54fbb4620d0e
sefaplbyk <sefa__2095@hotmail.com>
2023-07-31 23:55:30 +0300

    updt

--- Frontend-Mentor-Js/intro-component-with-signup-form-master/script.js
+++ Frontend-Mentor-Js/intro-component-with-signup-form-master/script.js
@@ -4,12 +4,31 @@ let lastNInput = document.querySelector('.fl>input');
 let emailInput = document.querySelector('.ei>input');
 let passwordInput = document.querySelector('.pi>input');
 
+let elements = document.querySelectorAll('.form>div>input')
 
 let btn = document.querySelector('.sb-btn');
 
 btn.onclick=(e)=>{
   e.preventDefault();
-  if(firstNInput.value.trim() === ""){
-    firstNInput.nextElementSibling.style.display = "inline";
+  elements.forEach((input)=>{
+    if (input.value.trim() === ""){
+      input.nextElementSibling.style.display = "inline"
+    }else {
+      input.nextElementSibling.style.display = "none"
+      return;
+    }
+  })
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  const isValidEmail = emailRegex.test(emailInput.value);
+
+  if (isValidEmail) {
+    emailInput.nextElementSibling.style.display = "none"
+
+    console.log('Valid email address');
+  } else {
+    emailInput.nextElementSibling.style.display = "inline"
+
+    console.log('Invalid email address');
   }
+console.log("object");
 }
