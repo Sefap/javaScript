0870e8eec3bf2bb3f754a9f5395751eb4358d7d2
sefaplbyk <sefa__2095@hotmail.com>
2023-08-10 18:46:19 +0300

    fix

--- Frontend-Mentor-Js/expenses-chart-component-main/script.js
+++ Frontend-Mentor-Js/expenses-chart-component-main/script.js
@@ -12,7 +12,7 @@ let html = `
 console.log(new Date().toString().slice(0,3));
 async function fetchData() {
   let currentDay = new Date().toString().slice(0,3).toLowerCase()
-  let classlist = ['flex','flex-col','items-center']
+  let classlist = ['flex','w-8','md:w-12','justify-end','flex-col','relative','items-center']
   try {
     const response = await fetch('./data.json');
     const names = await response.json();
@@ -20,14 +20,18 @@ async function fetchData() {
     names.forEach(name => {
       el = document.createElement('li');
       el.classList.add(...classlist)
-      const heightClass = `${Math.floor(name.amount)}`
+      const heightClass = `${Math.floor(name.amount)}` * 4
       console.log(heightClass);
     el.innerHTML = `
-    <div class= 'w-8 h-[${heightClass}px] bg-red-500 hover:opacity-50'></div>
-    <p>${name.day}</p>
+    <span class='amountSpan bottom-[${heightClass+40}px] p-0 md:p-1  rounded '>$${name.amount}</span>
+    <div class= 'w-full h-[${heightClass}px] bg-softred hover:opacity-50 rounded-md'></div>
+    <p class='font-extralight'>${name.day}</p>
   `;
-  name.day === currentDay ? el.firstElementChild.classList.add('current') :''
+  console.log(typeof name.day);
+  console.log(typeof currentDay);
+  name.day == currentDay ? el.firstElementChild.nextElementSibling.classList.add('current') :''
       charList.appendChild(el)
+     
     });
   } catch (error) {
     console.error('Veri alınırken bir hata oluştu:', error);
